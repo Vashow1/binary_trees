24022ae844ef1f7ee3dfc8f64ceee1b59f78fdb1
Vashow1 <muriukiaw1@gmail.com>
2022-11-15 05:28:00 +0000

    Finally figuring out a function to free every block

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+#include <stdio.h>
+/**
+ * binary_tree_delete - deletes an entire binary tree
+ * 
+ * @tree: a pointer to the root node of the tree to delete
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	binary_tree_t *nleft, *nright;
+
+	if (tree == NULL)
+		return;
+
+	nleft = tree->left;
+	nright = tree->right;
+
+	binary_tree_delete(nleft);
+	binary_tree_delete(nright);
+
+	free(tree);
+}

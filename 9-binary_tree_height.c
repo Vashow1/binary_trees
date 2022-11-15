c27343384be39737cabd468ad98c35aca5c56c19
Vashow1 <muriukiaw1@gmail.com>
2022-11-15 08:17:33 +0000

    Getting the height of the binary tree

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+
+/**
+ * MAX - compares two unsigned integers and returns the bigger
+ *
+ * @a: first size_t value
+ * @b: second size_t value
+ * Return: the bigger size_t
+ */
+
+size_t MAX(size_t a, size_t b)
+{
+        return((a >= b) ? a : b);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ *
+ * @tree: is a pointer to the root node of the tree to measure the height.
+ * Return: the height and if tree is NULL return 0
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	binary_tree_t *right, *left;
+	size_t leftHeight = 0, rightHeight = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	right = tree->right;
+	left = tree->left;
+
+	leftHeight = (left != NULL) ? (binary_tree_height(left) + 1) : 0;
+	rightHeight = (right != NULL) ? (binary_tree_height(right) + 1) : 0;
+	return (MAX(leftHeight, rightHeight));
+}
--- README.md
+++ README.md
@@ -5,3 +5,8 @@ This directory contains files to practice into binary trees and all associated s
 ### Pretty printing tree
 The link below directs to a helpful code that helps in visualizing the Binary trees
 http://stackoverflow.com/a/13755911/5184480
+
+
+## tests
+The files contained in the directory "tests" are curated to test the functionalities of the functions written in the main dir.
+Though the tests are really shallow and you are encouraged to develop more tests in order to test for the edge cases.

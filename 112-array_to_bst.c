87a97ca1166254a516722bd56d19cc34600d0e2a
Vashow1 <muriukiaw@yahoo.com>
2022-11-18 00:09:37 +0000

    Trying to fix for the requirement

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -12,9 +12,13 @@
 bst_t *array_to_bst(int *array, size_t size)
 {
 	bst_t *root = NULL;
+	size_t i = 0;
 
-	for (size_t i = 0; i < size; i++)
+	while (i < size)
+	{
 		bst_insert(&root, array[i]);
+		i++;
+	}
 
 	return (root);
 }
